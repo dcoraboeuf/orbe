e2f4c221e5c5426c1cdc39b629ab05c2262683b6
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-03-31 18:47:18 +0200

    Versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,16 @@
+plugins {
+    id("net.nemerosa.versioning") version "2.8.2"
+}
+
+version = versioning.info.display
+
+subprojects {
+    version = rootProject.version
+}
+
 allprojects {
     repositories {
         mavenCentral()
     }
     group = "orbe"
-    version = "dev"
 }
